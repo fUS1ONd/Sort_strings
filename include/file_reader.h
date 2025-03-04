d4ade4f25a777a34d024d06a5cb7c730ae81d128
fUS1ONd <koskriv2006@gmail.com>
2025-03-04 22:18:53 +0300

    forgot to commit

--- include/file_reader.h
+++ include/file_reader.h
@@ -4,4 +4,4 @@
 
 char **read_file(const char *filename, size_t line_counted);
 
-void free_file_content(char **lines, size_t line_counted);
+void free_file_content(char **array_of_strings, size_t line_counted);
